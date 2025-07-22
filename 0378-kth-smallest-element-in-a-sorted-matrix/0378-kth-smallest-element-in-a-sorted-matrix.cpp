e60dc05b7822c80ede507b862eb0034d1e934ad0
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-07-23 01:59:31 +0530

    Time: 7 ms (47.07%), Space: 29.7 MB (5.13%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,int>> p;
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        vector<p>arr;
-        for(int i=0;i<matrix.size();i++){
-            arr.push_back({matrix[i][0],{i,0}});
-        }
-        int n=matrix.size();
-        int m=matrix[0].size();
-        priority_queue<p,vector<p>,greater<p>> pq(arr.begin(),arr.end());
-        while(--k){
-            int val=pq.top().first;
-            int row=pq.top().second.first;
-            int col=pq.top().second.second;
-            pq.pop();
-            if(col+1<m){
-                pq.push({matrix[row][col+1],{row,col+1}});
+    int countless(vector<vector<int>> a,int mid,int n){
+        int r=n-1,c=0;
+        int cnt=0;
+        while(r>=0 && c<n){
+            if(a[r][c]<=mid){
+                cnt+=(r+1);
+                c++;
+            }else{
+                r--;
             }
         }
-        return pq.top().first;
+        return cnt;
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int ans=0;
+        int n=matrix.size();
+        int low=matrix[0][0],high=matrix[n-1][n-1];
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int cnt=countless(matrix,mid,n);
+            if(cnt>=k){
+                high=mid-1;
+                ans=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 };
