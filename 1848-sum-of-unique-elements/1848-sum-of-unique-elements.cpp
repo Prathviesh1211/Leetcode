338d0bedbfd4bd28ec5f93666f07c4d659857971
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-07-24 21:20:27 +0530

    Time: 0 ms (100%), Space: 10.8 MB (46.84%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.cpp
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
         unordered_map<int,int> mpp;
-        for(int i:nums)mpp[i]++;
         int sum=0;
-        for(auto it:mpp){
-            if(it.second==1)sum+=it.first;
+        for(int i:nums){
+            mpp[i]++;
+            if(mpp[i]==1)sum+=i;
+            else if(mpp[i]==2) sum-=i;
+        
         }
         return sum;
     }
