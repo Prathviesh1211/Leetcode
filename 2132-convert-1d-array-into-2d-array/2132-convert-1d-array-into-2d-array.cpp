3a8adc6e2fd030860448e733f5f258643c598e84
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-07-30 00:21:06 +0530

    Time: 27 ms (8.02%), Space: 94.4 MB (44.12%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -3,11 +3,10 @@ public:
     vector<vector<int>> construct2DArray(vector<int>& arr, int m, int n) {
         vector<vector<int>> ans(m,vector<int> (n));
         if(arr.size()!=m*n)return {};
-        int ind=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans[i][j]=arr[ind++];
-            }
+        for(int i=0;i<arr.size();i++){
+            int r=i/n;
+            int c=i%n;
+            ans[r][c]=arr[i];
         }
         return ans;
     }
