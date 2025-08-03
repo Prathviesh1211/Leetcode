00316865cf815d3729cdeb12a73ef7fa048506e4
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-08-03 23:32:21 +0530

    Time: 50 ms (52.06%), Space: 52.8 MB (34.05%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int longestSubstring(string s, int k) {
-        if(s.empty() || k>s.size())return 0;
-        int hash[26]={0};
-        for(auto it:s)hash[it-'a']++;
-        for(int i=0;i<s.size();i++){
-            if(hash[s[i]-'a']<k){
-                int left=longestSubstring(s.substr(0,i),k);
-                int right=longestSubstring(s.substr(i+1,s.size()),k);
-                return max(left,right);
+        if (s.empty() || k > s.size())
+            return 0;
+        int hash[26] = {0};
+        for (auto it : s)
+            hash[it - 'a']++;
+        for (int i = 0; i < s.size(); i++) {
+            if (hash[s[i] - 'a'] < k) {
+                int left = longestSubstring(s.substr(0, i), k);
+                int right = longestSubstring(s.substr(i + 1, s.size()), k);
+                return max(left, right);
             }
         }
-        return s.size();        
+        return s.size();
     }
 };
