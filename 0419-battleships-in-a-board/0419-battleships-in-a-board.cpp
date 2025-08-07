15abd7fdf1e56af991e372090f61ec525e505a89
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-08-07 14:46:00 +0530

    Time: 0 ms (100%), Space: 14.1 MB (12.6%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board, int r, int c) {
+    void bfs(vector<vector<char>>& board, int r, int c) {
         int n = board.size();
         int m = board[0].size();
-        if(r>=n || r<0 || c>=m || c<0 || board[r][c]!='X'){
-            return;
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        int dr[4] = {0, 0, -1, 1};
+        int dc[4] = {-1, 1, 0, 0};
+        while (!q.empty()) {
+            auto [row, col] = q.front();
+            q.pop();
+            board[row][col] = '.';
+            for (int i = 0; i < 4; i++) {
+                int nr = dr[i] + row;
+                int nc = dc[i] + col;
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && board[nr][nc] == 'X') {
+                    q.push({nr,nc});
+                }
+            }
         }
-        board[r][c]='.';
-        dfs(board,r-1,c);
-        dfs(board,r+1,c);
-        dfs(board,r,c-1);
-        dfs(board,r,c+1);
     }
 
     int countBattleships(vector<vector<char>>& board) {
@@ -20,7 +28,7 @@ public:
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (board[i][j] == 'X') {
-                    dfs(board, i, j);
+                    bfs(board, i, j);
                     ans++;
                 }
             }
