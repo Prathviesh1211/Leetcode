eae0688ddf9cac3c452dbb5d54f3967b8d9a4836
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-08-07 13:16:59 +0530

    Time: 27 ms (11.26%), Space: 105.4 MB (20.55%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
+    int dfs(vector<vector<int>>& grid, int r, int c, vector<vector<int>>& vis) {
+        int n = grid.size();
+        int m = grid[0].size();
+        if (r < 0 || r >= n || c < 0 || c >= m || grid[r][c] == 0)
+            return 1;
+        if (vis[r][c] == 1)
+            return 0;
+        vis[r][c] = 1;
+        int ans = 0;
+        ans += dfs(grid, r - 1, c, vis);
+        ans += dfs(grid, r + 1, c, vis);
+        ans += dfs(grid, r, c - 1, vis);
+        ans += dfs(grid, r, c + 1, vis);
+        return ans;
+    }
+
     int islandPerimeter(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int perimeter = 0;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    perimeter += 4;
-                    if (i > 0 && grid[i - 1][j] == 1)
-                        perimeter -= 1;
-                    if (i < n - 1 && grid[i + 1][j] == 1)
-                        perimeter -= 1;
-                    if (j > 0 && grid[i][j - 1] == 1)
-                        perimeter -= 1;
-                    if (j < m - 1 && grid[i][j + 1] == 1)
-                        perimeter -= 1;
+                    perimeter += dfs(grid, i, j, vis);
                 }
             }
         }
