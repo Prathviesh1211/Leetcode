a17fd560530137a7f1d6e17af15d1a16a28a3168
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-08-20 15:49:52 +0530

    Time: 0 ms (100%), Space: 13 MB (17.65%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,15 +3,15 @@ public:
     int hIndex(vector<int>& citations) {
         int ans=0;
         int n=citations.size();
+        vector<int> count(n+1,0);
+        for(int i:citations){
+            if(i>n)count[n]++;
+            else count[i]++;
+        }
+        int cnt=0;
         for(int i=n;i>=0;i--){
-            int cnt=0;
-            for(auto it:citations){
-                if(it>=i)cnt++;
-            }
-            if(cnt>=i){
-                ans=i;
-                break;
-            }
+            cnt+=count[i];
+            if(cnt>=i)return i;
         }
         return ans;
     }
