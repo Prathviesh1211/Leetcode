43c59e45348465cb3732d7fe9bbc735b421f0532
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-08-27 16:14:59 +0530

    Time: 21 ms (50.32%), Space: 26.3 MB (29.83%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int cnt = 1;
-        int n = grid.size();
-        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0)
-            return -1;
-        int m = grid[0].size();
-        vector<vector<int>> vis(n, (vector<int>(m, 0)));
-        queue<pair<int, int>> q;
-        vis[0][0] = 1;
-        q.push({0, 0});
-        int dr[8] = {-1, 1, 0, 0, -1, -1, 1, 1};
-        int dc[8] = {0, 0, -1, 1, -1, 1, -1, 1};
-        while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                auto [r, c] = q.front();
-                q.pop();
-
-                if (r == n - 1 && c == n - 1)
-                    return cnt;
-
-                for (int i = 0; i < 8; i++) {
-                    int nr = r + dr[i], nc = c + dc[i];
-                    if (nr >= 0 && nc >= 0 && nr < n && nc < n &&
-                        grid[nr][nc] == 0 && !vis[nr][nc]) {
-                        vis[nr][nc] = 1;
-                        q.push({nr, nc});
-                    }
+        if(grid[0][0]==1)return -1;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int> (n,0));
+        queue<tuple<int,int,int>> q;
+        q.push({0,0,1});
+        int dr[8]={0,0,1,-1,-1,-1,1,1};
+        int dc[8]={-1,1,0,0,-1,1,-1,1};
+        while(!q.empty()){
+            auto [r,c,dist]=q.front(); q.pop();
+            if(r==n-1 && c==m-1)return dist;
+            for(int i=0;i<8;i++){
+                int nr=dr[i]+r;
+                int nc=dc[i]+c;
+                if(nr>=0 && nr<n && nc<m && nc>=0 && grid[nr][nc]==0 && !vis[nr][nc]){
+                    vis[nr][nc]=1;
+                    q.push({nr,nc,dist+1});
                 }
             }
-            cnt++;
         }
         return -1;
     }
