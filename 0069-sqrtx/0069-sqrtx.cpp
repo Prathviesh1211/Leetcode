2826d342d016656b460735699787e260284243f6
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-09-19 00:32:32 +0530

    Time: 15 ms (10.94%), Space: 8.5 MB (50.54%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int ans=0;
+        for(long long i=1;i<=x;i++){
+            if(i*i<=x){
+                ans=i;
+            }else{
+                break;
+            }
+        }
+        return ans;
     }
 };
