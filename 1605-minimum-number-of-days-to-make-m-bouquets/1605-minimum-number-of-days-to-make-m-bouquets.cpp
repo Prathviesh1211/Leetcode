518f804108b4b3fd9d086efd260e0a4c2d85a4a3
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-09-20 18:26:44 +0530

    Time: 31 ms (16.74%), Space: 70.5 MB (76.01%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     int minDays(vector<int>& arr, int m, int k) {
-        int n=arr.size();
-        if((long long)m*k>n)return -1;
-        int s=*min_element(arr.begin(),arr.end());
-        int e=*max_element(arr.begin(),arr.end());
-        int ans=-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(isValid(arr,mid,m,k)){
-                ans=mid;
-                e=mid-1;
-            }else{
-                s=mid+1;
+        int n = arr.size();
+        if ((long long)m * k > n)
+            return -1;
+        int s = *min_element(arr.begin(), arr.end());
+        int e = *max_element(arr.begin(), arr.end());
+        int ans = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (isValid(arr, mid, m, k)) {
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
         return ans;
     }
-    bool isValid(vector<int>& arr,int mid,int m,int k){
-        int cnt=0;
-        int b=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]<=mid){
-                cnt++;
-            }else{
-                b+=(cnt/k);
-                cnt=0;
+    bool isValid(vector<int>& arr, int mid, int m, int k) {
+        int cnt = 0;
+        int flowers = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] <= mid) {
+                flowers++;
+                if (flowers == k) {
+                    cnt++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
             }
         }
-        b+=(cnt/k);
-        if(b<m)return false;
+        if (cnt < m)
+            return false;
         return true;
     }
-
 };
