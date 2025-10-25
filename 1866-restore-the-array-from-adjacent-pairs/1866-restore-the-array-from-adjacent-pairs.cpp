4ecfecf44f447b489afbaf3429aa5e5d46bb2d5a
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-10-26 01:08:51 +0530

    Time: 184 ms (71.57%), Space: 118.5 MB (73.31%) - LeetHub

--- 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.cpp
+++ 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.cpp
@@ -2,29 +2,40 @@ class Solution {
 public:
     vector<int> restoreArray(vector<vector<int>>& arr) {
         vector<int> ans;
-        unordered_map<int,vector<int>> adj;
-        for(auto it:arr){
-            int u=it[0];
-            int v=it[1];
+        unordered_map<int, vector<int>> adj;
+        for (auto it : arr) {
+            int u = it[0];
+            int v = it[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        int start=-1;
-        for(auto it:adj){
-            if(it.second.size()==1){
-                start=it.first;
+        int start = -1;
+        for (auto it : adj) {
+            if (it.second.size() == 1) {
+                start = it.first;
+                break;
             }
         }
-        dfs(ans,adj,start,-1);
+        ans.push_back(start);
+        while(ans.size()<adj.size()){
+            int curr=ans.back();
+            int next=adj[curr][0];
+            if (ans.size() > 1 && next == ans[ans.size() - 2]) {
+                next = adj[curr][1];
+            }
+            ans.push_back(next);
+        }
         return ans;
     }
 
-    void dfs(vector<int>& ans,unordered_map<int,vector<int>> &adj,int u,int prev){
-        ans.push_back(u);
-        for(auto v:adj[u]){
-            if(v!=prev){
-                dfs(ans,adj,v,u);
-            }
-        }
-    }
+    // void dfs(vector<int>& ans, unordered_map<int, vector<int>>& adj, int u,
+    //          int prev, vector<int>& vis) {
+    //     ans.push_back(u);
+    //     vis[u] = 1;
+    //     for (auto v : adj[u]) {
+    //         if (!vis[v]) {
+    //             dfs(ans, adj, v, u, vis);
+    //         }
+    //     }
+    // }
 };
