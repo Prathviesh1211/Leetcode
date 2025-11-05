fc1a86d13a4736217cdf2c8da6a2f9d3a691aca4
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-11-05 14:17:16 +0530

    Time: 7 ms (52.97%), Space: 11.4 MB (97.02%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    bool issame(vector<int> a, vector<int> b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i])
-                return false;
-        }
-        return true;
-    }
     vector<int> findAnagrams(string s, string p) {
         vector<int> ans;
-        vector<int> f1(26, 0), f2(26, 0);
-        for (char c : p)
-            f1[c - 'a']++;
-        int k = p.size();
-        if (k > s.size())
-            return {};
-        for (int i = 0; i < s.size(); i++) {
-            f2[s[i] - 'a']++;
-            if (i >= k) {
-                f2[s[i - k] - 'a']--;
-            }
-            if (i >= k - 1) {
-                if (issame(f1, f2))
-                    ans.push_back(i - k + 1);
+
+        int n=s.size();
+        int m=p.size();
+
+        if(m>n)return {};
+
+        vector<int> f1(26,0);
+        vector<int> f2(26,0);
+
+        for(char c:p)f2[c-'a']++;
+        
+        for(int i=0;i<m;i++)f1[s[i]-'a']++;
+
+        if(f1==f2)ans.push_back(0);
+
+        for(int i=m;i<n;i++){
+            f1[s[i]-'a']++;
+            f1[s[i-m]-'a']--;
+            if(f1==f2){
+                ans.push_back(i-m+1);
             }
         }
         return ans;
-
-        // T.C => O (n+k) 
-        // S.C => O (1)
     }
+
+    
 };
