b9a9688cc1a1afa3e47ddb6a70f8dc7db81ffa0e
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-11-09 02:45:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (66.13%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        // vector<int> freq(26,0);
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(s[i]==t[j]){
+                i++;
+                // j++;
+            }
+            j++;
+        }
+        return i==n;
+    }
+};
