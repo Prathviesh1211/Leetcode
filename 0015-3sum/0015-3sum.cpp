181d33eef15bdc66c368223030a276a404fba61e
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-11-12 18:47:26 +0530

    Time: 35 ms (97.89%), Space: 29.1 MB (48.4%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,6 +5,7 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
+            if(nums[i]>0)break;
             if(i>0 && nums[i]==nums[i-1])continue;
             int l=i+1;
             int r=n-1;
@@ -14,8 +15,9 @@ public:
                 else if(sum<0)l++;
                 else{
                     ans.push_back({nums[i],nums[l],nums[r]});
-                    l++;
+                    r--,l++;
                     while(nums[l]==nums[l-1] && l<r)l++;
+                    while(nums[r]==nums[r+1] && l<r)r--;
                 }
             }
         }   
