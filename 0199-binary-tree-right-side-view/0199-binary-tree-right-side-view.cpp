24f9d708bfb0b3ad429e85402915e6084ade1932
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2025-11-24 15:36:15 +0530

    Time: 0 ms (100%), Space: 15.2 MB (16.63%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -6,25 +6,29 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        if(!root)return ans;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,0});
-        while(!q.empty()){
-            int n=q.size();
-            int temp=0;
-            for(int i=0;i<n;i++){
-                auto [node,lvl]=q.front();
+        if (!root)
+            return ans;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        while (!q.empty()) {
+            int n = q.size();
+            int temp = 0;
+            for (int i = 0; i < n; i++) {
+                auto [node, lvl] = q.front();
                 q.pop();
-                 temp=node->val;
-                if(node->left)q.push({node->left,lvl+1});
-                if(node->right)q.push({node->right,lvl+1});
+                temp = node->val;
+                if (node->left)
+                    q.push({node->left, lvl + 1});
+                if (node->right)
+                    q.push({node->right, lvl + 1});
             }
             ans.push_back(temp);
         }
