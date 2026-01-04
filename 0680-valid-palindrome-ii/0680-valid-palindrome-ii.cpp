d3ceba0393c047d60e7045fa92f00f0a1171efea
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-01-04 23:27:02 +0530

    Time: 0 ms (100%), Space: 26.6 MB (40.77%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -7,13 +7,17 @@ public:
         }
         return true;
     }
+
     bool validPalindrome(string s) {
-        int i=0,j=s.size()-1;
+        int n=s.size();
+        int i=0,j=n-1;
         while(i<j){
-           if(s[i]!=s[j]){
-            return ispalind(s,i,j-1) || ispalind(s,i+1,j);
-           }      
-           i++,j--;
+            if(s[i]!=s[j]){
+                return (ispalind(s,i+1,j) || ispalind(s,i,j-1));
+            }else{
+                j--;
+                i++;
+            }
         }
         return true;
     }
