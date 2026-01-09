e62e0bfbacb84ff9ad391af9c08e5577495b7e45
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-01-10 01:12:01 +0530

    Time: 0 ms (100%), Space: 32.3 MB (6.6%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -5,14 +5,17 @@ public:
         int n=nums.size();
         int sum=0;
         int i=0;
-      for(int j=0;j<n;j++){
-        sum+=nums[j];
-        while(sum>=k){
-            ans=min(ans,j-i+1);
-            sum-=nums[i];
-            i++;
+        int j=0;
+        while(j<n){
+            sum+=nums[j];
+            while(sum>=k){
+                ans=min(ans,j-i+1);
+                sum-=nums[i];
+                i++;
+            }
+            j++;
         }
-      }
+
       return (ans==INT_MAX)?0:ans;  
     }
 };
