3bfd5ad6e14d0d7d0520fc4ce012bb6b4bed4486
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-01-12 18:51:29 +0530

    Time: 16 ms (32.15%), Space: 14.3 MB (27.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,15 +4,12 @@ public:
         int n=s.size();
         int ans=0;
         int i=0;
-        vector<int> arr(256,0);
+        unordered_set<char> st;
         for(int j=0;j<n;j++){
-            if(arr[s[j]]){
-                while(arr[s[j]]==1){
-                    arr[s[i]]--;
-                    i++;
-                }
+            while(st.count(s[j])){
+                st.erase(s[i++]);
             }
-            arr[s[j]]=1;
+            st.insert(s[j]);
             ans=max(ans,j-i+1);
         }
         return ans;
