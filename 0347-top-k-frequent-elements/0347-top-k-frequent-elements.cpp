cbf84a550319976af4ea0451a3d3b457897a2052
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-02-16 20:22:04 +0530

    Time: 0 ms (100%), Space: 17.8 MB (97%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,11 +6,12 @@ public:
         for(int i:nums){
             mpp[i]++;
         }
-        priority_queue<pair<int,int>> pq; 
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq; 
         for(auto it:mpp){
             pq.push({it.second,it.first});
+            if(pq.size()>k)pq.pop();
         }
-        while(k--){
+        while(!pq.empty()){
             ans.push_back(pq.top().second);
             pq.pop();
         }
