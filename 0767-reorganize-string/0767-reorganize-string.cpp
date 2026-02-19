d846318d3c3400ffb4afa103a8508a5d5a875323
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-02-19 18:06:47 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.99%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n=s.size();
+        string res="";
+        int ind=0;
+        unordered_map<char,int> mpp;
+        for(char c:s)mpp[c]++;
+        priority_queue<pair<int,char>> pq;
+        for(auto it:mpp)pq.push({it.second,it.first});
+        while(!pq.empty()){
+            pair p=pq.top();pq.pop();
+            if(res.empty() || res.back()!=p.second){
+                res+=p.second;
+                p.first--;
+                if(p.first>0)pq.push(p);
+            }else{
+                if(pq.empty())return "";
+                pair q=pq.top();pq.pop();
+                res+=q.second;
+                q.first--;
+                if(q.first>0)pq.push(q);
+                pq.push(p);
+            }
+        }
+        return res;
+    }
+};
