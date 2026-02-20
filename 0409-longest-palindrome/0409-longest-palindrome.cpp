b00de8e9b607612b0493367d7338a3c184da5e85
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-02-21 03:52:54 +0530

    Time: 0 ms (100%), Space: 9.1 MB (82.51%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int cnt=0;
-        int upper[26]={0};
-        int lower[26]={0};
-        for(char c:s){
-            if(islower(c))lower[c-'a']++;
-            else upper[c-'A']++;
-        }
+        vector<int> freq(128,0);
+        for(char c:s)freq[c]++;
         bool odd=false;
-        for(int i=0;i<26;i++){
-            if(lower[i]%2==0)cnt+=lower[i];
+        int ans=0;
+        for(int f:freq){
+            if(f%2==0)ans+=f;
             else{
-                cnt+=lower[i]-1;
+                ans+=f-1;
                 odd=true;
-            } 
-            if(upper[i]%2==0)cnt+=upper[i];
-            else{
-                cnt+=upper[i]-1;
-                odd=true;
-            } 
+            }
         }
-        if(odd)return odd+cnt;
-        return cnt;
+        if(odd)ans+=1;
+        return ans;
     }
 };
