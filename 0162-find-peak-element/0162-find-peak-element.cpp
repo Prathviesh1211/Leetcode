a482ca90f041ec615becf7ca4e225e795e6345ae
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-03-05 03:19:11 +0530

    Time: 0 ms (100%), Space: 12.7 MB (33.08%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,15 +9,13 @@ public:
     
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
-                return mid;
-            }
+    
             if(nums[mid]<nums[mid+1]){
                 s=mid+1;
             }else{
                 e=mid-1;
             }
         }
-        return -1;
+        return s;
     }
 };
