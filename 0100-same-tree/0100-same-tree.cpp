045d2c3237cb2a9cb494218c37853480d78babf6
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-03-19 04:23:13 +0530

    Time: 0 ms (100%), Space: 12.9 MB (46.82%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,8 +15,6 @@ public:
         if(!p && !q)return true;
         if(!p || !q)return false;
         if(p->val!=q->val)return false;
-        bool left=isSameTree(p->left,q->left); 
-        bool right=isSameTree(p->right,q->right); 
-        return left && right;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
