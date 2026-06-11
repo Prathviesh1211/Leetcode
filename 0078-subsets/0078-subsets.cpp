502d0b30b173e98b6cdb7bb50daec99ee3ba1a36
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-06-11 22:39:34 +0530

    Time: 0 ms (100%), Space: 9.8 MB (95.55%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void fn(vector<int>& arr, vector<vector<int>>& ans, vector<int> temp,
-            int ind) {
-        if (ind == arr.size()) {
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        if(nums.empty())return ans;
+        vector<int> temp;
+        fn(nums,ans,temp,0);
+        return ans;
+    }
+    void fn(vector<int>& nums,vector<vector<int>>& ans,vector<int>& temp,int ind){
+        if(ind>=nums.size()){
             ans.push_back(temp);
             return;
         }
-        fn(arr, ans, temp, ind + 1);
-        temp.push_back(arr[ind]);
-        fn(arr, ans, temp, ind + 1);
-    }
-
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        fn(nums, ans, temp, 0);
-        return ans;
+        temp.push_back(nums[ind]);
+        fn(nums,ans,temp,ind+1);
+        temp.pop_back();
+        fn(nums,ans,temp,ind+1);
     }
 };
