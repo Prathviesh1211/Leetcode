f3ac9425e59733065df80552839728839a8613a0
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-06-19 19:45:26 +0530

    Time: 29 ms (6.12%), Space: 45.8 MB (37.57%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& arr, int k) {
+        int n=arr.size();
+        int cnt=0;
+        sort(arr.begin(),arr.end());
+        int l=0,r=n-1;
+        while(l<=r){
+            if(arr[l]+arr[r]>k){
+                r--;
+            }else{
+                l++,r--;
+            }
+                cnt++;
+
+        }
+        return cnt;
+    }
+};
