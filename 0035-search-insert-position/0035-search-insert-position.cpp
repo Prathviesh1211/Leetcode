77388e33afd19c6ce42b49e9d481ac6d91a8d4a4
Prathviesh1211 <147980165+Prathviesh1211@users.noreply.github.com>
2026-06-27 23:56:58 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.57%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -5,10 +5,13 @@ public:
         int s=0,e=n-1;
         while(s<=e){
            int m=s+(e-s)/2;
-           if(nums[m]>k)e=m-1;
-           else if(nums[m]<k)s=m+1;
-           else return m;
+           if(nums[m]>=k){
+                ans=m;
+                e=m-1;
+            }else{
+                s=m+1;
+            }
         }
-        return s;
+        return ans;
     }
 };
