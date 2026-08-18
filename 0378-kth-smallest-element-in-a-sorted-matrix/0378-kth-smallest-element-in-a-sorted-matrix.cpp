a08bb931cec5ff4238dc82f755e7328a99487d5c
Prathviesh Naik <147980165+Prathviesh1211@users.noreply.github.com>
2026-08-18 14:13:18 +0530

    Time: 12 ms (18.63%), Space: 17.2 MB (55.02%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+    typedef pair<int,pair<int,int>> t;
     int kthSmallest(vector<vector<int>>& mat, int k) {
-        priority_queue<int> pq;
-        for(auto r:mat){
-            for(auto c:r){
-                pq.push(c);
-                if(pq.size()>k)pq.pop();       
-            }
+        priority_queue<t,vector<t>,greater<t>> pq;
+        int n=mat.size();
+        for(int i=0;i<mat[0].size();i++){
+            pq.push({mat[i][0],{i,0}});
         }
-        return pq.top();
+        while(k>1){
+            int val=pq.top().first;
+            auto [r,c]=pq.top().second;
+            pq.pop();
+            if(c+1<n){
+                pq.push({mat[r][c+1],{r,c+1}});
+            }
+            k--;
+        }
+        return pq.top().first;
     }
 };
