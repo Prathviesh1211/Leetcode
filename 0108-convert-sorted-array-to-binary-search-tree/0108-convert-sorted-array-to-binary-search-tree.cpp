61572b0f8e271179c1043ef3e0fc7f1b1382767e
Prathviesh Naik <147980165+Prathviesh1211@users.noreply.github.com>
2026-08-26 00:27:33 +0530

    Time: 2 ms (70.04%), Space: 22.9 MB (56.5%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -6,24 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* fn(vector<int>& arr, int s, int e) {
-        if (s > e)
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return fn(nums,0,nums.size()-1);
+    }
+    TreeNode* fn(vector<int>& nums,int l,int r) {
+        if(l>r){
             return nullptr;
-        int mid = s + (e - s) / 2;
-        TreeNode* root = new TreeNode(arr[mid]);
-        root->left = fn(arr, s, mid - 1);
-        root->right = fn(arr, mid + 1, e);
+        }
+        int mid=(l+r)/2;
+        TreeNode* root=new TreeNode(nums[mid]);
+        root->left=fn(nums,l,mid-1);
+        root->right=fn(nums,mid+1,r);
         return root;
     }
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        if (nums.empty())
-            return nullptr;
-        return fn(nums, 0, nums.size() - 1);
-    }
+
 };
